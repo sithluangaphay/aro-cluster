db5df7eb7602e3ec920176f10785b5c933438572
Sith Luangaphay <sluangap@excellus.com>
2023-01-05 16:40:17 +0000

    Updated storage.tf

--- LAB/storage.tf
+++ LAB/storage.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg-openshifttest01" {
 }
 
 resource "azurerm_storage_account" "st-openshift01" {
-  name                     = "st-openshift01"
+  name                     = "stopenshift01"
   resource_group_name      = azurerm_resource_group.rg-openshifttest01.name
   location                 = azurerm_resource_group.rg-openshifttest01.location
   account_tier             = "Standard"
