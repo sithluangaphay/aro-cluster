529ef3c360c262b5264e2eaf47cc5c655f136a79
Sith Luangaphay <sluangap@excellus.com>
2023-03-02 16:56:19 -0500

    update provider

--- LAB/provider.tf
+++ LAB/provider.tf
@@ -3,11 +3,23 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
+  
+  
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">=1.0.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.33.0"
+    }
+  }  
+  
+  
   }
 
-  required_version = ">= 1.1.0"
-}
+  
 
 provider "azurerm" {
   tenant_id = "29686bf7-7e4e-4498-b5b8-fe4162035737"
