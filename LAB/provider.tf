2c011694f573fc33b2f6389aeb58da3e911f2a3b
sithluangaphay <128738936+sithluangaphay@users.noreply.github.com>
2023-03-23 16:17:23 -0400

    Update provider.tf

--- LAB/provider.tf
+++ LAB/provider.tf
@@ -22,25 +22,6 @@ terraform {
   
 
 provider "azurerm" {
-  tenant_id = "29686bf7-7e4e-4498-b5b8-fe4162035737"
-  features {}
+    features {}
 }
 
-variable "environment" {
-  type = string
-}
-
-# Inject credentials separate from the ones used for the backend
-variable "client_id"{
-
-}
-
-# Inject credentials separate from the ones used for the backend
-variable "client_secret"{
-
-}
-
-# Inject credentials separate from the ones used for the backend
-variable "tenant_id"{
-
-}
